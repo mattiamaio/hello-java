3d62b1606403ced30d4bd7fa976cca07984697ca
Mattia Maio <mattia.maio10@gmail.com>
2021-11-12 14:11:33 +0100

    Update StampaSaluto.java
    seconda prova

--- src/org/generation/italy/StampaSaluto.java
+++ src/org/generation/italy/StampaSaluto.java
@@ -4,7 +4,7 @@ public class StampaSaluto {
 
 	public static void main(String[] args) {	  // metodo che esegue la classe
 		
-		System.out.println("Ciao a tutti");		  // espressione per stampare in console
+		System.out.println("Ciao a tutti");		 // espressione per stampare in console 
 
 	}
 
