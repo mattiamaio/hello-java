ba2039326d74b5c7cc07e6daad1ef4ffa9714f99
Mattia Maio <mattia.maio10@gmail.com>
2021-11-12 14:04:06 +0100

    Initial commit

---
+++ src/org/generation/italy/StampaSaluto.java
@@ -0,0 +1,11 @@
+package org.generation.italy;
+
+public class StampaSaluto {
+
+	public static void main(String[] args) {	  // metodo che esegue la classe
+		
+		System.out.println("Ciao a tutti");		  // espressione per stampare in console
+
+	}
+
+}
